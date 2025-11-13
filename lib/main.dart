9995921fc79b40a0eb2a730d851e8cbf47ecf631
Abdelrahman Hany <abdelrahmanhany20024@gmail.com>
2025-11-13 22:35:42 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Tager());
+}
+
+class Tager extends StatelessWidget {
+  const Tager({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
